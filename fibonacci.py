fa2bfbfb9bcd1a51aab3a9f70c7508dcce987dff
fernandonunex <fer.bsof@outlook.com>
2022-06-16 02:31:41 -0500

    Using lru_cache form functools to implement memoization

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,8 @@
 # 0,1,1,2,3,5,8,13,21,34
 # 0,1,2,3,4,5,6,7,8,9
+import functools
 import sys
+from functools import lru_cache
 
 def fibonacci_recursivo(n):
     """Funtion that return the n iterarion of fibonacci"""
@@ -22,10 +24,21 @@ def fibonacci_dinamico(n, memo={}):
 
         return result
 
+@lru_cache(maxsize = 1000)
+def fibonacci(n):
+    if n == 0 or n == 1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+
 if __name__ == '__main__':
     sys.setrecursionlimit(10002)
     n = int(input("Choose a number: "))
     #print(fibonacci_recursivo(n))
+    print("Fibo homemade")
     print(fibonacci_dinamico(n))
+
+    print("\nFibo for production")
+    print(fibonacci(n))
+    
 
     
